eb2c88a3962e64d6e0bc105400e961d97ac894b1
Adam Stankiewicz <41164873+AdamJStankiewicz@users.noreply.github.com>
2024-11-07 04:25:44 -0500

    basic storage
    this is not complete yet.

--- server.py
+++ server.py
@@ -2,9 +2,55 @@ from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 import uuid
 
+import sqlite3
+import os
+import json
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+connection = sqlite3.connect('syncstream.db', check_same_thread=False)
+cursor = connection.cursor()
+
+def init():
+    
+    server_info = {}
+    s = Storage('server_info.json',server_info)
+    s.initiate_storage
+
+        
+class Storage:
+
+    def __init__(self, server_info_path, server_info):
+        self.server_info_path = server_info_path
+        self.server_info = server_info
+        self.initiate_storage()
+
+    def initiate_storage(self):
+        if os.path.exists(self.server_info_path):
+            with open(self.server_info_path, 'r') as f:
+                self.server_info = json.load(f)
+        with open(self.server_info_path, 'w') as f:
+            json.dump(self.server_info, f)
+
+    def store(self, data):
+        with open(self.server_info_path, 'w') as f:
+            json.dump(data, f)
+
+    def get_data(self):
+        with open(self.server_info_path, 'r') as f:
+            data = json.load(f)
+        return data
+
+class db:
+    def initiate_db():
+        sql_command = """CREATE TABLE emp (
+        UUID VARCHAR(36),
+        username VARCHAR(50),
+        password VARCHAR(50)
+        );"""
+        cursor.execute(sql_command)
+        print("DATABASE INITIATED")
 
 class User:
     def __init__(self, user_id, username):
@@ -79,5 +125,7 @@ def serializeLobbySystem(lobbySystem):
 def main():
     print("Hello")
     return "SyncStream Online"
+
+init()
 
 socketio.run(app,host='0.0.0.0',port=1477, allow_unsafe_werkzeug=True, debug=False)
